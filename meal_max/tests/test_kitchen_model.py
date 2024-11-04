24290922292244f58fcad2d8daf15f5b61cf66d7
Haotian Liu <krisliuu@bu.edu>
2024-11-04 11:32:58 -0500

    complement unitests for kitchen model and fix bugs

--- meal_max/tests/test_kitchen_model.py
+++ meal_max/tests/test_kitchen_model.py
@@ -39,6 +39,10 @@ def mock_cursor(mocker):
 
     return mock_cursor  # Return the mock cursor so we can set expectations per test
 
+@pytest.fixture
+def sample_meal1():
+    return Meal(1, 'Spaghetti', 'Italian', 15, 'MED')
+
 def test_create_meal(mock_cursor):
     """Test creating a new meal in the catalog."""
 
@@ -162,15 +166,28 @@ def test_get_leaderboard(mock_cursor):
     mock_cursor.fetchall.return_value=[] # set return value of unitest
     get_leaderboard('wins') # call the function for testing
     expect_select_args=normalize_whitespace('SELECT id, meal, cuisine, price, difficulty, battles, wins, (wins * 1.0 / battles) AS win_pct FROM meals WHERE deleted = false AND battles > 0 ORDER BY wins DESC')
-    actual_select_args=mock_cursor.execute.call_args_list[0][0]
+    actual_select_args=normalize_whitespace(mock_cursor.execute.call_args_list[0][0][0])
     assert expect_select_args==actual_select_args, f'expected select args is {expect_select_args}, but got {actual_select_args}'
 
-def test_get_meal_by_id(mock_cursor):
-    mock_cursor.fetchone.return_value = None
-    get_meal_by_id(5)
+def test_get_leaderboard_None(mock_cursor):
+    with pytest.raises(ValueError, match='Invalid sort_by parameter: price'):
+        get_leaderboard('price')
+
+def test_get_meal_by_id_deleted(mock_cursor,sample_meal1):
+    mock_cursor.fetchone.return_value = [sample_meal1.id,sample_meal1.meal,sample_meal1.cuisine, sample_meal1.price, sample_meal1.difficulty,True]
+    with pytest.raises(ValueError, match= "Meal with ID 1 has been deleted" ):
+        get_meal_by_id(1)
+
+def test_get_meal_by_id_not_found(mock_cursor):
+    mock_cursor.fetchone.return_value=None
+    with pytest.raises(ValueError, match='Meal with ID 1 not found'):
+        get_meal_by_id(1)
+def test_get_meal_by_id(mock_cursor,sample_meal1):
+    mock_cursor.fetchone.return_value = [sample_meal1.id,sample_meal1.meal,sample_meal1.cuisine, sample_meal1.price, sample_meal1.difficulty,False]
+    get_meal_by_id(1)
     expect_select_args=normalize_whitespace("SELECT id, meal, cuisine, price, difficulty, deleted FROM meals WHERE id = ?")
-    expect_select_args_1=(5,)
-    actual_select_args_1=mock_cursor.execute.call_args_list[0][1]
-    actual_select_args=mock_cursor.execute.call_args_list[0][0]
+    expect_select_args_1=(1,)
+    actual_select_args_1=mock_cursor.execute.call_args_list[0][0][1]
+    actual_select_args=normalize_whitespace(mock_cursor.execute.call_args_list[0][0][0])
     assert expect_select_args==actual_select_args, f'expected select args is {expect_select_args}, but got {actual_select_args}'
     assert expect_select_args_1==actual_select_args_1, f'expected select args is {expect_select_args_1}, but got {actual_select_args_1}'
